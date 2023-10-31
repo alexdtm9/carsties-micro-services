3309f4c3c40e3c1b9f9643cbe935e0797b18c14e
alexdtm9 <a.m.talpau@icloud.com>
2023-10-31 19:17:14 +0100

    Add synchronous communication

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -19,20 +20,30 @@ public class DbInitializer
             .CreateAsync();
 
         var count = await DB.CountAsync<Item>();
-        if (count == 0)
-        {
-            Console.WriteLine("Seeding database...");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-            
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true,
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
-            
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
-            
-            await DB.InsertAsync(items);
-        }
+        // if (count == 0)
+        // {
+        //     Console.WriteLine("Seeding database...");
+        //     var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+        //     
+        //     var options = new JsonSerializerOptions
+        //     {
+        //         PropertyNameCaseInsensitive = true,
+        //         PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        //     };
+        //     
+        //     var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        //     
+        //     await DB.InsertAsync(items);
+        // }
+        
+        using var scope = app.Services.CreateScope();
+        
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionServiceHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+        
+        Console.WriteLine(items.Count + " returned from the auction service");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
     }
 }
